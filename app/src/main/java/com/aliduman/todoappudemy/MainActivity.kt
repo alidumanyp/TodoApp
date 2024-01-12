2d5c9484400e728cf314bb5c0735f854898c9606
alidumanyp <Alidumanyp@mail.com>
2024-01-12 22:25:06 +0300

    Initial commit

---
+++ app/src/main/java/com/aliduman/todoappudemy/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.aliduman.todoappudemy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aliduman.todoappudemy.ui.theme.TodoAppUdemyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodoAppUdemyTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+
+                }
+            }
+        }
+    }
+}
+
